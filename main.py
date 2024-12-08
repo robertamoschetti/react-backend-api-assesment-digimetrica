de205d0fdc61967634dcfb6abdd0d6d2aab5dcd0
Roberta Moschetti <robertamoschetti@MacBook-Air-di-Roberta.local>
2024-12-08 21:09:24 +0100

    Initial commit for backend

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-    print("Ciao, benvenuto nel mio progetto Python!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/report")
+async def get_report():
+    print("sono nel get")
+    with open("summary2.0.json", "r") as file:
+        data = json.load(file)
+
+    return data
